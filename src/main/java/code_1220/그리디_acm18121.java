95236eb61bfb5718d7a6a6d712ce1daaccd4b5b1
박경현 <tooli38@gmail.com>
2021-12-20 19:51:15 +0900

    1220코딩 스터디

---
+++ src/main/java/code_1220/그리디_acm18121.java
@@ -0,0 +1,20 @@
+package code_1220;
+
+import java.util.Scanner;
+
+public class 그리디_acm18121 {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        String input = sc.nextLine();
+        Long convertZero = getSwitch(input, "[0]+");
+        Long convertOne = getSwitch(input, "[1]+");
+        System.out.println(Math.min(convertZero,convertOne));
+    }
+
+    public static Long getSwitch(String input, String regex){
+        return input.replaceAll(regex,"2")
+                .chars()
+                .filter(a -> a == '2')
+                .count();
+    }
+}
